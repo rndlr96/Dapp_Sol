86052c61fb7f67d6c35fb1bbe49b0d8026745e07
JungGuIk <rndlr96@gmail.com>
2018-08-19 23:46:47 -0700

    [MOD] Update complete until interface

--- zombie/zombiefeeding.sol
+++ zombie/zombiefeeding.sol
@@ -2,7 +2,27 @@ pragma solidity ^0.4.19;
 
 import "./zombiefactory.sol";
 
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
 contract ZombieFeeding is ZombieFactory {
+
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  // `ckAddress`를 이용하여 여기에 kittyContract를 초기화한다
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);

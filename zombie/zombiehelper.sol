638385d2583d574f109a4ddf7cb1df72c1d9712b
JungGuIk <rndlr96@gmail.com>
2018-09-03 16:27:21 +0900

    [MOD] Update ownerOf modifier for checking owner

--- zombie/zombiehelper.sol
+++ zombie/zombiehelper.sol
@@ -24,13 +24,13 @@ contract ZombieHelper is ZombieFeeding {
     zombies[_zombieId].level++;
   }
 
-  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  // 1. Modify this function to use `ownerOf`:
+  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) ownerOf(_zombieId){
     zombies[_zombieId].name = _newName;
   }
 
-  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  // 2. Do the same with this function:
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) ownerOf(_zombieId){
     zombies[_zombieId].dna = _newDna;
   }
 
